61512545a2eee8aa45fa0fdc31b9154482535b6f
kkmiiaa <mxg7y3ansl+github@gmail.com>
2025-08-17 22:27:50 +0900

    fix: downgrade tailwindcss to v3 to resolve build issue

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+module.exports = config;
