6732a17733a65a59e5b676a1aff34bd2fcae1d89
kkmiiaa <mxg7y3ansl+github@gmail.com>
2025-08-18 17:31:23 +0900

    update

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,8 +1,18 @@
+import Image from 'next/image'; // Imageコンポーネントをインポート
+
 const Header = () => (
-  <header className="w-full max-w-md p-4">
+  <header className="w-full max-w-md">
     <div className="flex justify-between items-center text-white">
-      <h1 className="text-2xl font-bold">adult_matching</h1>
-      <button className="px-4 py-2 text-sm font-bold text-white rounded-xl border border-white/50 hover:bg-white/20 transition-colors duration-200">
+      {/* <h1>adult_matching</h1> の代わりに画像を表示 */}
+      <Image
+        src="/seiheki_lab.png" // publicディレクトリからのパス
+        alt="Seiheki Lab Logo"
+        width={230} // 画像の幅を大きく設定
+        height={100} // 画像の高さを大きく設定
+        priority // LCP改善のため、優先的に読み込む
+        draggable="false" // 画像を選択できないようにする
+      />
+      <button className="p-4 py-2 mx-2 text-sm font-bold text-white rounded-xl border border-white/50 hover:bg-white/20 transition-colors duration-200">
         ログイン
       </button>
     </div>
