4b88e9d503d2e0aea930d81ef22ddca94dbd228e
kkmiiaa <mxg7y3ansl+github@gmail.com>
2025-08-23 23:46:16 +0900

    feat(frontend): Set header margin to 0 for mobile view

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -43,7 +43,7 @@ const Header = ({ cardWidth }: { cardWidth: number | undefined }) => {
   };
 
   return (
-    <header id="main-header" className={`mx-auto mt-4 mb-2 ${isMobile ? 'p-4 bg-gradient-to-r from-[#C4C8E3] via-[#D7D1E3] to-[#F7D7E0] to-[#F8DBB9] shadow-md w-full' : ''}`} style={!isMobile ? { width: cardWidth ? `${cardWidth}px` : 'auto' } : {}}>
+    <header id="main-header" className={`mx-auto ${isMobile ? 'p-4 bg-gradient-to-r from-[#C4C8E3] via-[#D7D1E3] to-[#F7D7E0] to-[#F8DBB9] shadow-md w-full' : 'mt-4 mb-2'}`} style={!isMobile ? { width: cardWidth ? `${cardWidth}px` : 'auto' } : {}}>
       <div className="flex justify-between items-center text-white mx-auto" style={!isMobile ? { width: cardWidth ? `${cardWidth}px` : 'auto' } : {}}>
         <Image
           src="/seiheki_lab.png"
