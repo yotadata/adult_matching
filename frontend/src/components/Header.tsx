e7deb6b61f791a63a8de017b2bbc026dd91ecfc1
kkmiiaa <mxg7y3ansl+github@gmail.com>
2025-08-23 23:06:24 +0900

    fix(frontend): Ensure header inner div respects cardWidth on mobile

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -44,7 +44,7 @@ const Header = ({ cardWidth }: { cardWidth: number | undefined }) => {
 
   return (
     <header id="main-header" className={`w-full mx-auto mt-4 mb-2 ${isMobile ? 'p-4 bg-gradient-to-r from-[#C4C8E3] via-[#D7D1E3] to-[#F7D7E0] to-[#F8DBB9] shadow-md' : ''}`} style={{ maxWidth: cardWidth ? `${cardWidth}px` : 'auto' }}>
-      <div className="flex justify-between items-center text-white mx-auto" style={!isMobile ? { width: cardWidth ? `${cardWidth}px` : 'auto' } : {}}>
+      <div className="flex justify-between items-center text-white mx-auto" style={{ maxWidth: cardWidth ? `${cardWidth}px` : 'auto' }}>
         <Image
           src="/seiheki_lab.png"
           alt="Seiheki Lab Logo"
