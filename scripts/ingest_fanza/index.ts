6c8a01071014e8f77480fd65622db0a149965ac8
kkmiiaa <mxg7y3ansl+github@gmail.com>
2026-05-18 11:10:44 +0900

    feat: FANZAアニメ動画をingest対象に追加

--- scripts/ingest_fanza/index.ts
+++ scripts/ingest_fanza/index.ts
@@ -557,8 +557,8 @@ async function main() {
   console.log(`Fetching videos released between ${gteReleaseDate} and ${lteReleaseDate}...`);
 
   const sources = [
-    { service: 'digital', floor: 'videoa',         source: 'FANZA' },
-    { service: 'doujin',  floor: 'digital_doujin', source: 'FANZA_DOUJIN' },
+    { service: 'digital', floor: 'videoa', source: 'FANZA' },
+    { service: 'digital', floor: 'anime',  source: 'FANZA_ANIME' },
   ];
 
   let totalSuccess = 0;
